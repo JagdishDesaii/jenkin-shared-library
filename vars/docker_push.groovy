7fe92ccb898bbda8018eab99d28fb64dd919b4e5
Jagdish Desaii <147185343+JagdishDesaii@users.noreply.github.com>
2026-06-24 15:19:54 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -7,7 +7,7 @@ def call(String project, String imageTag, String dockerHubUser) {
     )]) {
 
         sh """
-            docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}
+            echo ${DOCKER_PASS} | docker login -u ${DOCKER_USER} --password-stdin
             docker push ${dockerHubUser}/${project}:${imageTag}
         """
     }
